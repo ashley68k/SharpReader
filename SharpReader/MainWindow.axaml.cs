e5aeba731b4c37cadcd1e507b8331cc4ac2cec93
Ashley Scott <ashley68k@proton.me>
2025-08-21 19:23:02 -0400

    Rename project to SharpReader

--- SharpReader/MainWindow.axaml.cs
+++ SharpReader/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using Avalonia.Controls;
 
-namespace pdf2text;
+namespace SharpReader;
 
 public partial class MainWindow : Window
 {
--- SharpReader/Program.cs
+++ SharpReader/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Avalonia;
 using System;
 
-namespace pdf2text;
+namespace SharpReader;
 
 class Program
 {
